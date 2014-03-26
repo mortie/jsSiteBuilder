4ce0807fcfe8ee939fdfefee52493fbbd1534d4c
Martin Dørum Nygaard <martin@mortie.org>
2014-03-26 11:01:16 +0100

    prevention against acidentally XSS'ing yourself through stupid setting values

--- admin/scripts/updateSettings.php
+++ admin/scripts/updateSettings.php
@@ -2,9 +2,9 @@
 	foreach($_POST as $key=>$val) {
 		$exploded = explode("->", $key);
 		if (empty($exploded[1])) {
-			$settings->$exploded[0] = $val;
+			$settings->$exploded[0] = htmlentities($val);
 		} else {
-			$settings->$exploded[0]->$exploded[1] = $val;
+			$settings->$exploded[0]->$exploded[1] = htmlentities($val);
 		}
 	}
 
