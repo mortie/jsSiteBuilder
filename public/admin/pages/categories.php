2f015222cc2c01f525dc26283e565dbf7b6723c6
Martin Dørum Nygaard <martid0311@gmail.com>
2014-05-14 18:03:09 +0200

    fixed some issues related to the recent renaming, plus some unrelated issues regarding categories

--- public/admin/pages/categories.php
+++ public/admin/pages/categories.php
@@ -2,7 +2,7 @@
 	requirePassword();
 ?>
 
-<form id="form" method="post" action="?s=updateSettings">
+<form id="form" method="post" action="?s=updateCategories">
 <?php
 	$types = $mysqli->query("SELECT * FROM categories");
 
